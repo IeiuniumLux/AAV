62bf9e046a7b354cd5c67cbd924309664a21d8e2
abencomo <Alfredo.Bencomo@nasa.gov>
2015-05-09 18:41:11 -0700

    This version doesn't require the OpenCV Manager app

--- src/ioio/aav/AAVActivity.java
+++ src/ioio/aav/AAVActivity.java
@@ -49,6 +49,7 @@ import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.View;
 import android.view.WindowManager;
 
 public class AAVActivity extends IOIOActivity implements CvCameraViewListener2 {
@@ -56,36 +57,42 @@ public class AAVActivity extends IOIOActivity implements CvCameraViewListener2 {
 
 	static final double MIN_CONTOUR_AREA = 100;
 
-	private Mat rgba_image;
+	private Mat _rgbaImage;
 
-	private JavaCameraView opencv_camera_view;
-	private ActuatorController main_controller;
+	private JavaCameraView _opencvCameraView;
+	private ActuatorController _mainController;
 
-	volatile double contour_area = 7;
-	volatile Point center_point = new Point(-1, -1);
+	volatile double _contourArea = 7;
+	volatile Point _centerPoint = new Point(-1, -1);
 	Point _screenCenterCoordinates = new Point(-1, -1);
 	int _countOutOfFrame = 0;
 
-	Mat hsv_mat;
-	Mat processed_mat;
-	Mat dilated_mat;
-	Scalar lower_threshold;
-	Scalar upper_threshold;
+	Mat _hsvMat;
+	Mat _processedMat;
+	Mat _dilatedMat;
+	Scalar _lowerThreshold;
+	Scalar _upperThreshold;
 	final List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
 
-	SharedPreferences shared_preferences;
-	GestureDetector gesture_detector;
-	static int tracking_color = 0;
+	SharedPreferences _sharedPreferences;
+	GestureDetector _gestureDetector;
+	static int _trackingColor = 0;
+
+	static {
+		if (!OpenCVLoader.initDebug()) {
+			Log.d("ERROR", "Unable to load OpenCV");
+		}
+	}
 
 	private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
 		@Override
 		public void onManagerConnected(int status) {
 			switch (status) {
 			case LoaderCallbackInterface.SUCCESS: {
-				opencv_camera_view.enableView();
-				hsv_mat = new Mat();
-				processed_mat = new Mat();
-				dilated_mat = new Mat();
+				_opencvCameraView.enableView();
+				_hsvMat = new Mat();
+				_processedMat = new Mat();
+				_dilatedMat = new Mat();
 			}
 				break;
 			default: {
@@ -106,28 +113,28 @@ public class AAVActivity extends IOIOActivity implements CvCameraViewListener2 {
 		setContentView(R.layout.main);
 
 		PreferenceManager.setDefaultValues(this, R.xml.settings, false);
-		shared_preferences = PreferenceManager.getDefaultSharedPreferences(this);
-		tracking_color = Integer.parseInt(shared_preferences.getString(getString(R.string.color_key), "0"));
+		_sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+		_trackingColor = Integer.parseInt(_sharedPreferences.getString(getString(R.string.color_key), "0"));
 
-		if (tracking_color == 0) {
-			lower_threshold = new Scalar(60, 100, 30); // Green
-			upper_threshold = new Scalar(130, 255, 255);
-		} else if (tracking_color == 1) {
-			lower_threshold = new Scalar(160, 50, 90); // Purple
-			upper_threshold = new Scalar(255, 255, 255);
-		} else if (tracking_color == 2) {
-			lower_threshold = new Scalar(1, 50, 150); // Orange
-			upper_threshold = new Scalar(60, 255, 255);
+		if (_trackingColor == 0) {
+			_lowerThreshold = new Scalar(60, 100, 30); // Green
+			_upperThreshold = new Scalar(130, 255, 255);
+		} else if (_trackingColor == 1) {
+			_lowerThreshold = new Scalar(160, 50, 90); // Purple
+			_upperThreshold = new Scalar(255, 255, 255);
+		} else if (_trackingColor == 2) {
+			_lowerThreshold = new Scalar(1, 50, 150); // Orange
+			_upperThreshold = new Scalar(60, 255, 255);
 		}
 
-		opencv_camera_view = (JavaCameraView) findViewById(R.id.aav_activity_surface_view);
-		opencv_camera_view.setCvCameraViewListener(this);
+		_opencvCameraView = (JavaCameraView) findViewById(R.id.aav_activity_surface_view);
+		_opencvCameraView.setCvCameraViewListener(this);
 
-		opencv_camera_view.setMaxFrameSize(176, 144);
-		main_controller = new ActuatorController();
+		_opencvCameraView.setMaxFrameSize(352, 288); // (176, 144); //(320, 240); <-Callback buffer is too small for these resolutions.
+		_mainController = new ActuatorController();
 		_countOutOfFrame = 0;
 
-		gesture_detector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
+		_gestureDetector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
 			@Override
 			public void onLongPress(MotionEvent e) {
 				startActivityForResult(new Intent(getApplicationContext(), SettingsActivity.class), 0);
@@ -136,31 +143,26 @@ public class AAVActivity extends IOIOActivity implements CvCameraViewListener2 {
 	}
 
 	@Override
-	public boolean onTouchEvent(MotionEvent event) {
-		return gesture_detector.onTouchEvent(event);
-	};
-
-	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
-		tracking_color = Integer.parseInt(shared_preferences.getString(getString(R.string.color_key), "0"));
+		_trackingColor = Integer.parseInt(_sharedPreferences.getString(getString(R.string.color_key), "0"));
 
-		switch (tracking_color) {
+		switch (_trackingColor) {
 		case 0: // Green
-			lower_threshold.set(new double[] { 60, 100, 30, 0 });
-			upper_threshold.set(new double[] { 130, 255, 255, 0 });
+			_lowerThreshold.set(new double[] { 60, 100, 30, 0 });
+			_upperThreshold.set(new double[] { 130, 255, 255, 0 });
 			break;
 		case 1: // Purple
-			lower_threshold.set(new double[] { 160, 50, 90 });
-			upper_threshold.set(new double[] { 255, 255, 255, 0 });
+			_lowerThreshold.set(new double[] { 160, 50, 90 });
+			_upperThreshold.set(new double[] { 255, 255, 255, 0 });
 			break;
 		case 2: // Orange
-			lower_threshold.set(new double[] { 1, 50, 150 });
-			upper_threshold.set(new double[] { 60, 255, 255, 0 });
+			_lowerThreshold.set(new double[] { 1, 50, 150 });
+			_upperThreshold.set(new double[] { 60, 255, 255, 0 });
 			break;
 		default:
-			lower_threshold.set(new double[] { 60, 100, 30, 0 });
-			upper_threshold.set(new double[] { 130, 255, 255, 0 });
+			_lowerThreshold.set(new double[] { 60, 100, 30, 0 });
+			_upperThreshold.set(new double[] { 130, 255, 255, 0 });
 			break;
 		}
 	}
@@ -169,59 +171,71 @@ public class AAVActivity extends IOIOActivity implements CvCameraViewListener2 {
 	public void onResume() {
 		super.onResume();
 
-		OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_7, this, mLoaderCallback);
+		// OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_10, this, mLoaderCallback);
+		mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+		hideNavigationBar();
 	}
 
 	@Override
 	public void onPause() {
 		super.onPause();
 
-		if (opencv_camera_view != null)
-			opencv_camera_view.disableView();
+		if (_opencvCameraView != null)
+			_opencvCameraView.disableView();
 	}
 
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
 
-		if (opencv_camera_view != null)
-			opencv_camera_view.disableView();
+		if (_opencvCameraView != null)
+			_opencvCameraView.disableView();
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN);
+		return _gestureDetector.onTouchEvent(event);
+	}
+
+	private void hideNavigationBar() {
+		getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN);
 	}
 
 	@Override
 	public void onCameraViewStarted(int width, int height) {
-		rgba_image = new Mat(height, width, CvType.CV_8UC4);
-		_screenCenterCoordinates.x = rgba_image.size().width / 2;
-		_screenCenterCoordinates.y = rgba_image.size().height / 2;
+		_rgbaImage = new Mat(height, width, CvType.CV_8UC4);
+		_screenCenterCoordinates.x = _rgbaImage.size().width / 2;
+		_screenCenterCoordinates.y = _rgbaImage.size().height / 2;
 	}
 
 	@Override
 	public void onCameraViewStopped() {
-		rgba_image.release();
-		center_point.x = -1;
-		center_point.y = -1;
-		main_controller.reset();
+		_rgbaImage.release();
+		_centerPoint.x = -1;
+		_centerPoint.y = -1;
+		_mainController.reset();
 	}
 
 	@Override
 	public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
 		synchronized (inputFrame) {
 
-			rgba_image = inputFrame.rgba();
+			_rgbaImage = inputFrame.rgba();
 
 			double current_contour;
 
 			// In contrast to the C++ interface, Android API captures images in the RGBA format.
 			// Also, in HSV space, only the hue determines which color it is. Saturation determines
 			// how 'white' the color is, and Value determines how 'dark' the color is.
-			Imgproc.cvtColor(rgba_image, hsv_mat, Imgproc.COLOR_RGB2HSV_FULL);
+			Imgproc.cvtColor(_rgbaImage, _hsvMat, Imgproc.COLOR_RGB2HSV_FULL);
 
 			// int ch[] = {0, 0};
 			// MatOfInt fromTo = new MatOfInt(ch);
-			// Mat hue = new Mat(rgba_image.size(), CvType.CV_8U);
+			// Mat hue = new Mat(_rgbaImage.size(), CvType.CV_8U);
 			// List<Mat> hsvlist = new ArrayList<Mat>();
 			// List<Mat> huelist = new ArrayList<Mat>();
-			// hsvlist.add(0, hsv_mat);
+			// hsvlist.add(0, _hsvMat);
 			// huelist.add(0, hue);
 			// Core.mixChannels(hsvlist, huelist, fromTo);
 			// hue = huelist.get(0);
@@ -230,37 +244,37 @@ public class AAVActivity extends IOIOActivity implements CvCameraViewListener2 {
 			// Log.e("dst", String.valueOf(dst.total()));
 			//
 			// List<Mat> chan = new ArrayList<Mat>();
-			// Core.split( hsv_mat, chan);
-			// Mat H = hsv_mat.row(0);
+			// Core.split( _hsvMat, chan);
+			// Mat H = _hsvMat.row(0);
 			// Mat binary = new Mat();
 			// Imgproc.threshold(H, binary, 1 /*ignored for otsu*/, 255, Imgproc.THRESH_OTSU);
 
-			Core.inRange(hsv_mat, lower_threshold, upper_threshold, processed_mat);
+			Core.inRange(_hsvMat, _lowerThreshold, _upperThreshold, _processedMat);
 
-			// Imgproc.dilate(processed_mat, dilated_mat, new Mat());
-			Imgproc.erode(processed_mat, dilated_mat, new Mat());
-			Imgproc.findContours(dilated_mat, contours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+			// Imgproc.dilate(_processedMat, _dilatedMat, new Mat());
+			Imgproc.erode(_processedMat, _dilatedMat, new Mat());
+			Imgproc.findContours(_dilatedMat, contours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
 			MatOfPoint2f points = new MatOfPoint2f();
-			contour_area = 7;
+			_contourArea = 7;
 			for (int i = 0, n = contours.size(); i < n; i++) {
 				current_contour = Imgproc.contourArea(contours.get(i));
-				if (current_contour > contour_area) {
-					contour_area = current_contour;
-					// contours.get(i).convertTo(points, CvType.CV_32FC2); // contours.get(x) is a single MatOfPoint, but to use minEnclosingCircle we need to pass a MatOfPoint2f so we need to do a
+				if (current_contour > _contourArea) {
+					_contourArea = current_contour;
+					contours.get(i).convertTo(points, CvType.CV_32FC2); // contours.get(x) is a single MatOfPoint, but to use minEnclosingCircle we need to pass a MatOfPoint2f so we need to do a
 					// conversion
 				}
 			}
-			if (!points.empty() && contour_area > MIN_CONTOUR_AREA) {
-				Imgproc.minEnclosingCircle(points, center_point, null);
-				// Core.circle(rgba_image, center_point, 3, new Scalar(255, 0, 0), Core.FILLED);
-				Core.circle(rgba_image, center_point, (int) Math.round(Math.sqrt(contour_area / Math.PI)), new Scalar(255, 0, 0), 3, 8, 0);// Core.FILLED);
+			if (!points.empty() && _contourArea > MIN_CONTOUR_AREA) {
+				Imgproc.minEnclosingCircle(points, _centerPoint, null);
+				// Core.circle(_rgbaImage, _centerPoint, 3, new Scalar(255, 0, 0), Core.FILLED);
+				Core.circle(_rgbaImage, _centerPoint, (int) Math.round(Math.sqrt(_contourArea / Math.PI)), new Scalar(255, 0, 0), 3, 8, 0);// Core.FILLED);
 			}
 			contours.clear();
 
 			// double area = 1;
 			// Mat circles = new Mat();
-			// Imgproc.GaussianBlur(processed_mat, processed_mat, new Size(9, 9), 2, 2);
-			// Imgproc.HoughCircles(processed_mat, circles, Imgproc.CV_HOUGH_GRADIENT, 2, processed_mat.rows() / 4, 100, 50, 10, 100);
+			// Imgproc.GaussianBlur(_processedMat, _processedMat, new Size(9, 9), 2, 2);
+			// Imgproc.HoughCircles(_processedMat, circles, Imgproc.CV_HOUGH_GRADIENT, 2, _processedMat.rows() / 4, 100, 50, 10, 100);
 			//
 			// // Draw the circles detected
 			// int rows = circles.rows();
@@ -270,20 +284,20 @@ public class AAVActivity extends IOIOActivity implements CvCameraViewListener2 {
 			// if (circleData.length > 0) {
 			// circles.get(0, 0, circleData); // Points to the first element and reads the whole thing into circleData
 			// int radius = 0; for (int i = 0; i < circleData.length; i = i + 3) {
-			// center_point.x = circleData[i];
-			// center_point.y = circleData[i + 1];
+			// _centerPoint.x = circleData[i];
+			// _centerPoint.y = circleData[i + 1];
 			// radius = (int) Math.round(circleData[2]);
 			// area = Math.PI * (radius * radius);
-			// if (area > contour_area) {
-			// contour_area = area;
+			// if (area > _contourArea) {
+			// _contourArea = area;
 			// }
 			// }
-			// if (contour_area > MIN_CONTOUR_AREA)
-			// Core.circle(rgba_image, center_point, radius, new Scalar(255, 0, 0), 3);
+			// if (_contourArea > MIN_CONTOUR_AREA)
+			// Core.circle(_rgbaImage, _centerPoint, radius, new Scalar(255, 0, 0), 3);
 			// }
 
 		}
-		return rgba_image;
+		return _rgbaImage;
 	}
 
 	/**
@@ -319,17 +333,17 @@ public class AAVActivity extends IOIOActivity implements CvCameraViewListener2 {
 		protected void setup() throws ConnectionLostException, InterruptedException {
 
 			try {
-				pwm_values = main_controller.getPWMValues();
+				pwm_values = _mainController.getPWMValues();
 
-				pwm_pan = ioio_.openPwmOutput(10, 100); // 9 shield
-				pwm_tilt = ioio_.openPwmOutput(6, 100); // 5 shield
+				pwm_pan = ioio_.openPwmOutput(Arduino.PIN_9, 100); // 9 shield
+				pwm_tilt = ioio_.openPwmOutput(Arduino.PIN_5, 100); // 5 shield
 				pwm_motor = ioio_.openPwmOutput(27, 100); // screw terminal
-				pwm_front_wheels = ioio_.openPwmOutput(12, 100); // 11 shield
+				pwm_front_wheels = ioio_.openPwmOutput(Arduino.PIN_11, 100);
 
-				front_left_IR = ioio_.openAnalogInput(40); // A/D 0 shield
-				side_left_IR = ioio_.openAnalogInput(41); // A/D 1 shield
-				front_right_IR = ioio_.openAnalogInput(43); // A/D 3 shield
-				side_right_IR = ioio_.openAnalogInput(42); // A/D 4 shield
+				front_left_IR = ioio_.openAnalogInput(Arduino.PIN_AD0);
+				side_left_IR = ioio_.openAnalogInput(Arduino.PIN_AD1);
+				front_right_IR = ioio_.openAnalogInput(Arduino.PIN_AD2);
+				side_right_IR = ioio_.openAnalogInput(Arduino.PIN_AD3);
 
 				pwm_pan.setPulseWidth((int) pwm_values[0]);
 				pwm_tilt.setPulseWidth((int) pwm_values[1]);
@@ -353,48 +367,48 @@ public class AAVActivity extends IOIOActivity implements CvCameraViewListener2 {
 		public void loop() throws ConnectionLostException {
 
 			try {
-				synchronized (main_controller) {
+				synchronized (_mainController) {
 
-					if (contour_area > MIN_CONTOUR_AREA) {
+					if (_contourArea > MIN_CONTOUR_AREA) {
 
-						main_controller.updatePanTiltPWM(_screenCenterCoordinates, center_point);
-						main_controller.ir_sensors.updateIRSensorsVoltage(front_left_IR.getVoltage(), front_right_IR.getVoltage(), side_left_IR.getVoltage(), side_right_IR.getVoltage());
+						_mainController.updatePanTiltPWM(_screenCenterCoordinates, _centerPoint);
+						_mainController.ir_sensors.updateIRSensorsVoltage(front_left_IR.getVoltage(), front_right_IR.getVoltage(), side_left_IR.getVoltage(), side_right_IR.getVoltage());
 
-						// if (!main_controller.irSensors.isBacking(front_left_IR.getVoltage(), front_right_IR.getVoltage(), side_left_IR.getVoltage(), side_right_IR.getVoltage(), contour_area)) {
+						// if (!_mainController.irSensors.isBacking(front_left_IR.getVoltage(), front_right_IR.getVoltage(), side_left_IR.getVoltage(), side_right_IR.getVoltage(), _contourArea)) {
 
 						if (pwm_counter > 8) {
-							main_controller.updateMotorPWM(contour_area);
+							_mainController.updateMotorPWM(_contourArea);
 							pwm_counter = 0;
 						} else {
 							pwm_counter++;
 
-							// if (main_controller.irSensors.checkIRSensors())
+							// if (_mainController.irSensors.checkIRSensors())
 							// pwm_counter = 10;
 						}
 						// }
 
-						// main_controller.irSensors.setIRSensorsVoltage(front_left_IR.getVoltage(), front_right_IR.getVoltage(), side_left_IR.getVoltage(), side_right_IR.getVoltage());
+						// _mainController.irSensors.setIRSensorsVoltage(front_left_IR.getVoltage(), front_right_IR.getVoltage(), side_left_IR.getVoltage(), side_right_IR.getVoltage());
 
-						// if (main_controller.irSensors.isBacking()) {
-						// main_controller.calculatePanTiltPWM(_screenCenterCoordinates, center_point);
+						// if (_mainController.irSensors.isBacking()) {
+						// _mainController.calculatePanTiltPWM(_screenCenterCoordinates, _centerPoint);
 						// } else {
 						// // MUST BE IN THIS ORDER
-						// main_controller.calculatePanTiltPWM(_screenCenterCoordinates, center_point);
-						// // main_controller.calculateWheelsPWM();
-						// main_controller.calculateMotorPWM(contour_area);
-						// main_controller.irSensors.checkIRSensors();
+						// _mainController.calculatePanTiltPWM(_screenCenterCoordinates, _centerPoint);
+						// // _mainController.calculateWheelsPWM();
+						// _mainController.calculateMotorPWM(_contourArea);
+						// _mainController.irSensors.checkIRSensors();
 						// }
 
 						_countOutOfFrame = 0;
 					} else {
 						_countOutOfFrame++;
 						if (_countOutOfFrame > 20) {
-							main_controller.reset();
+							_mainController.reset();
 							_countOutOfFrame = pwm_counter = 0;
 						}
 					}
 
-					pwm_values = main_controller.getPWMValues();
+					pwm_values = _mainController.getPWMValues();
 
 					pwm_pan.setPulseWidth((int) pwm_values[0]);
 					pwm_tilt.setPulseWidth((int) pwm_values[1]);
---
+++ src/ioio/aav/Arduino.java
@@ -0,0 +1,35 @@
+package ioio.aav;
+
+public class Arduino {
+
+	// Arduino Mapping
+	public static int PIN_0 = 5;
+	public static int PIN_1 = 4;
+	public static int PIN_2 = 23;
+	public static int PIN_3 = 3;
+	public static int PIN_4 = 24;
+	public static int PIN_5 = 6;
+	public static int PIN_6 = 7;
+	public static int PIN_7 = 18;
+	public static int PIN_8 = 19;
+	public static int PIN_9 = 10;
+	public static int PIN_10 = 11;
+	public static int PIN_11 = 12;
+	public static int PIN_12 = 13;
+	public static int PIN_13 = 14;
+
+	public static int PIN_AD0 = 40;
+	public static int PIN_AD1 = 41;
+	public static int PIN_AD2 = 42;
+	public static int PIN_AD3 = 43;
+	public static int PIN_AD4 = 44;
+	public static int PIN_AD5 = 45;
+	public static int PIN_SDA = 1;
+	public static int PIN_SCL = 2;
+
+	/*
+	 * 31 PPSi, PPSo Unmapped 32 PPSi, PPSo Unmapped 34 PPSi, PPSo Unmapped 35 PPSi, PPSo Unmapped 36 PPSi, PPSo Unmapped 37 PPSi, PPSo Unmapped 38 PPSi, PPSo Unmapped
+	 * 
+	 * 46 PPSi, PPSo Unmapped
+	 */
+}
